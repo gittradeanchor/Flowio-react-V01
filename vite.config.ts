027fb9006f6ac425a0c913df4e2a70ebc05cf125
gittradeanchor <billing@tradeanchor.com.au>
2025-12-02 12:15:20 +1100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,15 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// 1. Get your repository name from GitHub (e.g. if your repo is https://github.com/user/my-landing-page, the name is 'my-landing-page')
+// 2. Replace '/FlowioLanding-React/' below with '/YOUR-REPO-NAME/'
+
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // Add this line here!
+      base: '/Flowio-react-V01/', 
+      
       server: {
         port: 3000,
         host: '0.0.0.0',
